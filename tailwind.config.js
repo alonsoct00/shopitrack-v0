c67944d9645d00a80094e683c4376b06d27b0823
Alonso CT <g.alonso.ct@gmail.com>
2026-09-11 14:33:04 -0600

    update home

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
   },
   daisyui: {
     themes: [{ shopitrack: {
-      primary: '#1264e8', secondary: '#092c78', accent: '#ff6848', neutral: '#122958', 'base-100': '#ffffff', 'base-200': '#eef7ff', 'base-300': '#d9e9fa', info: '#1264e8', success: '#12a879', warning: '#f4b740', error: '#de4b5f',
+      primary: '#1264e8', secondary: '#092c78', accent: '#ff6848', neutral: '#122958', 'base-100': '#ffffff', 'base-200': '#eef7ff', 'base-300': '#d9e9fa', 'base-content': '#082663', info: '#1264e8', success: '#12a879', warning: '#f4b740', error: '#de4b5f',
     } }],
   },
   plugins: [require('daisyui')],
